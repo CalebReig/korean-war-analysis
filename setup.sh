96032f2aac9d1bb365a142d06886f37b540796a8
CalebReig <44947576+CalebReig@users.noreply.github.com>
2021-09-21 21:53:33 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"reigadacaleb@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
